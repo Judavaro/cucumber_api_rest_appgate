93d371fe1f2519331610f00a417068f3ce28d971
johanht <johan@kometsales.com>
2021-07-08 23:36:43 -0500

    we loop the datatable

--- src/test/java/steps/CreateUserNumericNameSteps.java
+++ src/test/java/steps/CreateUserNumericNameSteps.java
@@ -1,13 +1,14 @@
 package steps;
 
+import static org.testng.Assert.assertTrue;
+
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import java.security.KeyStore.Entry;
 import java.util.List;
 import model.User;
-
-
 
 public class CreateUserNumericNameSteps extends User{
   private String first_name;
@@ -15,7 +16,7 @@ public class CreateUserNumericNameSteps extends User{
   private String email;
   private String avatar;
 
-  private List<List<String>> usersList;
+  private DataTable userDataTable;
 
   @Given("I create a new user with many emails")
   public void iCreateANewUserWithManyEmails() {
@@ -23,20 +24,28 @@ public class CreateUserNumericNameSteps extends User{
 
   @When("I send the <email>")
   public void iSendTheEmail(DataTable users) {
-    usersList = users.asLists(String.class);
+    userDataTable = users;
   }
 
   @Then("I want to know if the email send has correctly format")
   public void iWantToKnowIfTheEmailSendHasCorrectlyFormat() {
 
-    int index = 0;
-    for (List<String> user: usersList) {
+    userDataTable.rows(.)forEach(aDataModel -> {
+      String username = aDataModel.user;
+      String password = aDataModel.password;
+    });
 
-      //
+    for (String user : usersList) {
 
-      assertTrue(User.validateEmail(user.toArray()[2]));
-      System.out.println(">>>>");
+      System.out.println(">>>> "+user.);
+
     }
+
+
+      //assertTrue(User.validateEmail((String) user.toArray()[2]));
+      //System.out.println(">>>> "+User.validateEmail((String) user.toArray()[2]));
+
+
 
     /*newUser.put("name", userName);
     newUser.put("job", jobName);
